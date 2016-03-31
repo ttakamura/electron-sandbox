dd0d4b880c1155a2f502554961083718abcfbd11
Tatsuya Takamura <takamura@tonchidot.com>
2016-03-31 12:31:25 +0900

    [from now] Thu Mar 31 12:31:25 JST 2016

--- hello-react/src/bootstrap.js
+++ hello-react/src/bootstrap.js
@@ -1,13 +1,8 @@
 'use strict';
 
 import 'babel-polyfill';
-
-//import babelRegister from 'babel-register';
-//babelRegister();
-
 import React from 'react';
 import { render } from 'react-dom';
-
 import {Main} from './components/main';
 
 console.log("Hello react from bootstrap");
